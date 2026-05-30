71ca4b4be49a4d2fbca100cc5bc719144bf3b890
ashok <ashok.bscity@gmail.com>
2026-05-31 00:06:16 +0530

    added new set of tests/specs

---
+++ tests/demo/mytest.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+test("Should load homepage with correct title", async ({ page }) => {
+  //Go to the Home page
+  await page.goto("https://katalon-demo-cura.herokuapp.com/");
+
+  //2.Assert if the title is correct
+  await expect(page).toHaveTitle("CURA Healthcare Service");
+
+  //3.Assert header text
+  await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
+});
+
+test("Should do something", { tag: "@smoke" }, async ({ page }, testInfo) => {
+  // steps..
+  await page.locator("//h1").click();
+});
+
+test.only(
+  "Should demo locators",
+  { tag: "@smoke" },
+  async ({ page }, testInfo) => {
+    // `page.getBy*() ` and `page.locator()` methods returns the `locator` object.
+    // The above method not to be `awaited`.
+    // The type of locator is an `object`.
+    // Locators are LAZY until an action is fired on them.
+    await page.goto("https://katalon-demo-cura.herokuapp.com/");
+
+    //2. Click on the Make appointment
+    let makeAppmtBtn = page.getByRole("link", { name: "Invalid Locator" })
+    // console.log(`>>> The type of locator: ${typeof makeAppmtBtn}, The value of the locator is: ${JSON.stringify(makeAppmtBtn)}`)
+    await makeAppmtBtn.click();
+    // await expect(page.getByText("Please login to make")).toBeVisible();
+    await page.getByRole('heading', { name: 'We Care About Your Health' }).click();
+
+
+  },
+);
---
+++ tests/functional/login.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Login functionality", () => {
+  test.beforeEach("Go to login page", async ({ page }) => {
+    await page.goto("https://katalon-demo-cura.herokuapp.com/");
+    await expect(page).toHaveTitle("CURA Healthcare Service");
+    await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
+
+    //2. Click on the Make appointment
+    await page.getByRole("link", { name: "Make appointment" }).click();
+    await expect(page.getByText("Please login to make")).toBeVisible();
+  });
+
+  test("Should login successfully", async ({ page }) => {
+    //Successful Login
+    await page.getByLabel("Username").fill("John Doe");
+    await page.getByLabel("Password").fill("ThisIsNotAPassword");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    //Assert a text
+    await expect(page.locator("h2")).toContainText("Make Appointment");
+  });
+
+  test("Should prevent login with incorrect creds", async ({ page }) => {
+    //1.Launch URL and assert title and header.
+
+    //3. Unsuccessful Login
+    await page.getByLabel("Username").fill("John Smith");
+    await page.getByLabel("Password").fill("ThisIsNotAPassword");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    //4. Assert a error Message
+    await expect(page.locator("#login")).toContainText(
+      "Login failed! Please ensure the username and password are valid.",
+    );
+  });
+});
